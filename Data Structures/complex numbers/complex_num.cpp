f87e017acc756390df8b8e0ac9b39eb9e61660ca
McCoy <mccoybecker@gmail.com>
2018-08-01 11:02:26 -0700

    Update

--- Data Structures/complex numbers/complex_num.cpp
+++ Data Structures/complex numbers/complex_num.cpp
@@ -4,15 +4,21 @@ using namespace std;
 class complex
 {
     public:
+        double get_r() {return r; }
+        void set_r (double newr) { r = newr ;}
+        double get_i() {return i; }
+        void set_i (double newi) { i = newi ;}
+
+    private:
         double r,i;
 };
 
 int main(){
     complex z,c;
-    z.r = 3.5;
-    z.i = 2;
-    c.r = 4;
-    c.i = 4;
-    cout << "z is (" << z.r << ", " << z.i << ")\n";
+    z.set_r(3.5);
+    z.set_i(2);
+    c.set_r(4);
+    c.set_i(4);
+    cout << "z is (" << z.get_r()<< ", " << z.get_i() << ")\n";
     return(0);
 }
